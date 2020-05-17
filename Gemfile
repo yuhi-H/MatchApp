390405f6ef6d793d60087208a4a7cb9a893a01e5
ゆーひ <ahiru@karushifu.local>
2020-05-17 18:40:14 +0900

    mini_magicka導入　プロフィール画面作成

--- Gemfile
+++ Gemfile
@@ -15,7 +15,7 @@ gem 'bootstrap', '~> 4.3.1'
 gem 'jquery-rails'
 gem "font-awesome-sass"
 gem 'carrierwave', '~> 1.0'
-
+gem 'mini_magick'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/uploaders/img_name_uploader.rb
+++ app/uploaders/img_name_uploader.rb
@@ -1,7 +1,7 @@
 class ImgNameUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -13,6 +13,9 @@ class ImgNameUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  version :thumb do
+    process resize_to_fit: [400, 400]
+  end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
@@ -35,9 +38,9 @@ class ImgNameUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
